f19a8ed61ee520f7aea1b098a0f457bb11fefd80
Tony Kosseify <tonykosseify123@gmail.com>
2021-05-14 13:47:00 +0300

    starting with the sidebar component

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Sidebar.css' ;
+
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className='sidebar-header'>
+        <div className='sidebar-header-right'>
+
+        </div>
+      </div>
+    </div>
+  )
+} ;
+
+
+export default Sidebar ;
