7fb8c0ef4e2ff4722d61a0cc6128a0f4b42c9cfb
Tony Kosseify <tonykosseify123@gmail.com>
2021-05-17 16:37:10 +0300

    sidebar component icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Sidebar from './Components/Sidebar' ;
+import Chat from './Components/Chat' ;
+
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <div className='app-body'>
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './Sidebar.css' ;
-
+import ChatIcon from '@material-ui/icons/Chat' ;
+import MoreVertIcon from '@material-ui/icons/MoreVert' ;
+import DonuLargeIcon from '@material-ui/icons/DonutLarge' ;
+import { Avatar , IconButton } from '@material-ui/core' ;
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <div className='sidebar-header'>
+        <Avatar />
         <div className='sidebar-header-right'>
-
+          <IconButton>
+            <DonuLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
       </div>
+
+      <div className='sidebar-search'>
+
+      </div>
+
     </div>
   )
 } ;
